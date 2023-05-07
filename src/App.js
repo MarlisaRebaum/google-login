040cfd5eb1a68f58a58eba13891c79469e84dc23
marlisar <marlisa@rebaum.com>
2023-05-07 13:50:37 -0700

    added route to Register page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from './auth/Login';
+import Register from './auth/Register';
 import './App.css';
 
 export default function App() {
@@ -8,6 +9,7 @@ export default function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </Router>
     </div>

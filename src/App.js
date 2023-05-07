60285f2bff5470b0b0fddbaa032352049d039f50
marlisar <marlisa@rebaum.com>
2023-05-07 13:57:50 -0700

    installed bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './auth/Login';
 import Register from './auth/Register';
 import './App.css';
